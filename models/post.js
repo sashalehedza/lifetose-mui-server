8fc517b68fae7e93ff6f745965ffc8a4c266f079
Sasha Lehedza <sashalehedza@gmail.com>
2022-11-26 15:25:39 +0200

    fix

--- models/post.js
+++ models/post.js
@@ -1,43 +1,46 @@
 import mongoose from 'mongoose'
 import { commentSchema } from './comment.js'
 
-const postSchema = mongoose.Schema({
-  title: String,
-  description: String,
-  name: String,
-  creator: String,
-  price: Number,
-  tags: [String],
-  imageFile: String,
-  createdAt: {
-    type: Date,
-    default: new Date(),
+const postSchema = mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    name: String,
+    creator: String,
+    price: Number,
+    tags: [String],
+    imageFile: String,
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    discount: Number,
+    saleCount: Number,
+    saleDiscount: Number,
+    //reviews: { type: [commentSchema], default: [] },
+    reviews: [],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
   },
-  likes: {
-    type: [String],
-    default: [],
-  },
-  discount: Number,
-  saleCount: Number,
-  saleDiscount: Number,
-  //reviews: { type: [commentSchema], default: [] },
-  reviews: [],
-  rating: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  numReviews: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-})
+  { timestamps: true }
+)
 
 const PostModal = mongoose.model('Post', postSchema)
 
